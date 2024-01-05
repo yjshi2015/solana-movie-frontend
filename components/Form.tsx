57394e6dda79dd2016d5ce346c5fe2517e2ddf05
yjshi <495054021@qq.com>
2024-01-05 15:56:52 +0800

    调整程序ID

--- components/Form.tsx
+++ components/Form.tsx
@@ -5,7 +5,7 @@ import { Box, Button, FormControl, FormLabel, Input, NumberDecrementStepper, Num
 import * as web3 from '@solana/web3.js'
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 
-const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
+const MOVIE_REVIEW_PROGRAM_ID = 'DiNeQbEPMZspM7uF973SQhPKY5WVdz8DZ1Z4uVwTaCsv'
 
 export const Form: FC = () => {
     const [title, setTitle] = useState('')
